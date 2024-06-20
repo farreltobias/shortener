ed09acffe19ceab14d232a52e22fff8cc0f79069
Guilherme Farrel de Souza <garcia.farrel@gmail.com>
2024-06-20 20:00:58 -0300

    feat(infra): :passport_control: add auth module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,13 +3,14 @@ import { Module } from '@nestjs/common'
 import { GetUrlByCodeUseCase } from '@/domain/shorten/application/use-cases/get-url-by-code'
 import { ShortenUrlUseCase } from '@/domain/shorten/application/use-cases/shorten-url'
 
+import { CryptographyModule } from '../cryptography/cryptography.module'
 import { DatabaseModule } from '../database/database.module'
 import { EnvModule } from '../env/env.module'
 import { RedirectController } from './controllers/redirect.controller'
 import { ShortenUrlController } from './controllers/shorten-url.controller'
 
 @Module({
-  imports: [DatabaseModule, EnvModule],
+  imports: [DatabaseModule, CryptographyModule, EnvModule],
   controllers: [ShortenUrlController, RedirectController],
   providers: [ShortenUrlUseCase, GetUrlByCodeUseCase],
 })
