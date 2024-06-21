b2f7b7ad75b530eefafaffcb25a4fb1426cf04aa
Guilherme Farrel de Souza <garcia.farrel@gmail.com>
2024-06-21 16:13:13 -0300

    fix(infra): :bug: change redis service to URL

--- src/infra/cache/redis/redis.service.ts
+++ src/infra/cache/redis/redis.service.ts
@@ -6,11 +6,7 @@ import { EnvService } from '@/infra/env/env.service'
 @Injectable()
 export class RedisService extends Redis implements OnModuleDestroy {
   constructor(envService: EnvService) {
-    super({
-      host: envService.get('REDIS_HOST'),
-      port: envService.get('REDIS_PORT'),
-      db: envService.get('REDIS_DB'),
-    })
+    super(envService.get('REDIS_URL'))
   }
 
   async onModuleDestroy() {
--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -4,9 +4,7 @@ export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   JWT_PRIVATE_KEY: z.string(),
   JWT_PUBLIC_KEY: z.string(),
-  REDIS_HOST: z.string().optional().default('127.0.0.1'),
-  REDIS_PORT: z.coerce.number().optional().default(6379),
-  REDIS_DB: z.coerce.number().optional().default(0),
+  REDIS_URL: z.string().optional().default('redis://127.0.0.1:6379/0'),
   PORT: z.coerce.number().optional().default(3333),
   DOMAIN: z.string().url().optional().default('http://localhost:3333'),
 })
